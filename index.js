c693dba7d9e2c2185a3f88ca630743034e0f2f4a
gabbymushi <gabrielpatrickmushi@gmail.com>
2019-06-10 21:25:22 +0300

    add body to options

--- index.js
+++ index.js
@@ -6,21 +6,28 @@ const request = require('request');
 
 /* request authorization token from tigo */
 function getToken() {
-    /*Prepare request options */
-    const options = {
-        url : 'http://accessgwtest.tigo.co.tz:8080/PFIXERS2DM-GetToken',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'Cache-Control': 'no-cache',
-
-        }
-    };
-    /*Prepare request body */
-    const requestBody = {
+      /*Prepare request body */
+      const body = {
         username: 'PFixers',
         password: 'y62QXLn',
         grant_type: 'password'
     };
+    /*Prepare request options */
+    const options = {
+        url : 'http://accessgwtest.tigo.co.tz:8080/PFIXERS2DM-GetToken',
+        method:'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Cache-Control': 'no-cache',
+
+        },
+        body:body
+    };
+  
+    /*Make a request */
+    request(options,function(error,response,body){
+
+    })
 
 
 
