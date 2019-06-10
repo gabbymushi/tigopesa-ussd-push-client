a228062d3d3852b68884c5796da1ab84bd9b277c
gabbymushi <gabrielpatrickmushi@gmail.com>
2019-06-10 17:24:48 +0300

    prepare request body

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ const request = require('request');
 
 /* request authorization token from tigo */
 function getToken(){
-    
+    /*Prepare request body */
+    const requestBody = {
+        username: 'PFixers',
+        password: 'y62QXLn',
+        grant_type: 'password'
+    };
+
 }
 
