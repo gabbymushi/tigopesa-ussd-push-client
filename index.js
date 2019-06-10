adcd3a3fa974fc218a9c93563e01cfbc0f01c205
gabbymushi <gabrielpatrickmushi@gmail.com>
2019-06-10 21:30:58 +0300

    handle error on response

--- index.js
+++ index.js
@@ -6,27 +6,29 @@ const request = require('request');
 
 /* request authorization token from tigo */
 function getToken() {
-      /*Prepare request body */
-      const body = {
+    /*Prepare request body */
+    const body = {
         username: 'PFixers',
         password: 'y62QXLn',
         grant_type: 'password'
     };
     /*Prepare request options */
     const options = {
-        url : 'http://accessgwtest.tigo.co.tz:8080/PFIXERS2DM-GetToken',
-        method:'POST',
+        url: 'http://accessgwtest.tigo.co.tz:8080/PFIXERS2DM-GetToken',
+        method: 'POST',
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded',
             'Cache-Control': 'no-cache',
 
         },
-        body:body
+        body: body
     };
-  
-    /*Make a request */
-    request(options,function(error,response,body){
 
+    /*Make a request */
+    request(options, function (error, response, body) {
+        if (error) {
+         return false
+        }
     })
 
 
