1a921549c099e6c3459be1888fe989d172ad1450
gabbymushi <gabrielpatrickmushi@gmail.com>
2019-06-11 17:04:46 +0300

    refactor

--- index.js
+++ index.js
@@ -3,8 +3,6 @@
 
 /* dependencies */
 const request = require('request');
-const querystring = require('querystring');
-const { waterfall } = require('async');
 
 /* request authorization token from tigo */
 const getToken = () => {
@@ -23,45 +21,48 @@ const getToken = () => {
             'Cache-Control': 'no-cache',
 
         },
-        body: querystring.stringify(body)
+        form: body
     };
 
     /*Make a request */
-    return request(options, function (error, response, body) {
-        if (error) {
-            return false
-        }
-        return body.data.access_token;
-    })
-
-    const charge = (options, done) => {
-        /*request token*/
-        const token = getToken();
-        /*prepare body*/
-        const body = {
-            CustomerMSISDN: options.msisdn,
-            BillerMSISDN: options.businessNumber,
-            Amount: options.amount,
-            // Remarks: options.description,
-            ReferenceID: options.reference,
-        };
-        const requestOptions = {
-            url: 'http://accessgwtest.tigo.co.tz:8080/PFIXERS2DM-PushBillpay',
-            headers: {
-                'Content-Type': 'application/json',
-                'Cache-Control': 'no-cache',
-                Authorization: 'bearer ' + token,
-                Username: 'PFixers',
-                Password: 'y62QXLn'
-
-            },
-            body: body
-        };
-        /*Make a request */
-        return request(requestOptions, function (error, response, body) {
-            return done(error, body);
+    return new Promise(function (resolve, reject) {
+        request(options, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                resolve(body);
+            } else {
+                reject(error);
+            }
         })
-    }
+    });
+}
+async function charge(options, done) {
+    /*request token*/
+    const token = JSON.parse(await getToken()).access_token;
+    const body = {
+        CustomerMSISDN: options.msisdn,
+        BillerMSISDN: options.businessNumber,
+        Amount: options.amount,
+        // Remarks: options.description,
+        ReferenceID: options.reference,
+    };
+    const requestOptions = {
+        url: 'http://accessgwtest.tigo.co.tz:8080/PFIXERS2DM-PushBillpay',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Cache-Control': 'no-cache',
+            Authorization: 'bearer ' + token,
+            Username: 'PFixers',
+            Password: 'y62QXLn'
 
+        },
+        body: body,
+        json: true
+
+    };
+    /*Make a request */
+    return request(requestOptions, function (error, response, body) {
+        return done(error, body);
+    })
 }
 module.exports = exports = { charge, getToken }
