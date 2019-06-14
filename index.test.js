285bcdd6d206a46fbba521e549e74dd50e51ca01
gabbymushi <gabrielpatrickmushi@gmail.com>
2019-06-14 12:52:06 +0300

    fix test

--- index.test.js
+++ index.test.js
@@ -1,7 +1,8 @@
 var expect=require('chai').expect;
 const {getToken}=require('./index');
 describe('Test token',function(){
-    it('It should return token as a string',function(){
+    it('It should return token as a string',function(done){
         expect(getToken).to.be.string;
+        done();
     })
 })
