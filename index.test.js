7c7fe2b575c8360af6bd8ec3cf62d762af3e6419
gabbymushi <gabrielpatrickmushi@gmail.com>
2019-06-14 13:13:20 +0300

    add mocha exit

--- index.test.js
+++ index.test.js
@@ -1,8 +1,10 @@
 var expect=require('chai').expect;
 const {getToken}=require('./index');
+describe('Token', function() {
 describe('Test token',function(){
     it('It should return token as a string',function(done){
         expect(getToken).to.be.string;
-        done();
+       return done();
     })
-})
+});
+});
