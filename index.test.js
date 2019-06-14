bc5da0b8914429d5c47acdc5fb42e86ff5dcc2d1
gabbymushi <gabrielpatrickmushi@gmail.com>
2019-06-14 13:14:36 +0300

    remove done

--- index.test.js
+++ index.test.js
@@ -2,9 +2,8 @@ var expect=require('chai').expect;
 const {getToken}=require('./index');
 describe('Token', function() {
 describe('Test token',function(){
-    it('It should return token as a string',function(done){
+    it('It should return token as a string',function(){
         expect(getToken).to.be.string;
-       return done();
     })
 });
 });
