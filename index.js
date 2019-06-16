26a661e8385e1e96d8defa98e592f07b00ce8204
gabbymushi <gabrielpatrickmushi@gmail.com>
2019-06-16 21:49:03 +0300

    export login

--- index.js
+++ index.js
@@ -167,4 +167,4 @@ async function charge(options, done) {
         return done(error, feedback);
     });
 }
-module.exports = exports = { charge, getToken }
+module.exports = exports = { charge, login }
